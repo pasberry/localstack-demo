de45ea382546faac29443bc951eb3628e13da84a
Phillip Asberry <phillip.j.asberry@gmail.com>
2023-01-12 19:45:14 -0500

    Initial commit

---
+++ test/local_stack_inrfa.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LocalStackInrfa from '../lib/local_stack_inrfa-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/local_stack_inrfa-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LocalStackInrfa.LocalStackInrfaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
